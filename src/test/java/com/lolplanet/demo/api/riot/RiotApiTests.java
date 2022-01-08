77c2f4c8f126769b3fbad591da8dcd11ddd388b2
Jaewook Choi <eari100@naver.com>
2022-01-09 00:50:24 +0900

    test: riot API에서 데이터 가져오는 코드 수정

--- src/test/java/com/lolplanet/demo/api/riot/RiotApiTests.java
+++ src/test/java/com/lolplanet/demo/api/riot/RiotApiTests.java
@@ -1,8 +1,11 @@
 package com.lolplanet.demo.api.riot;
 
+import com.lolplanet.demo.api.riot.dto.SummonerDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 public class RiotApiTests {
@@ -11,12 +14,14 @@ public class RiotApiTests {
     RiotApi riotApi;
 
     @Test
-    public void SummonerApi_test() {
+    public void 라이엇API에서_소환사_데이터_가져오기() {
         String summonerName = "한남동의 황제";
         String delimitedUrl = String.format("lol/summoner/v4/summoners/by-name/%s", summonerName);
 
-        Object object = riotApi.callApi(delimitedUrl);
+        SummonerDto summonerDto = riotApi.callApi(delimitedUrl, SummonerDto.class);
 
-        System.out.println("결과 값 반환: "+object.toString());
+        System.out.println(summonerName + " 의 소환사 정보: "+summonerDto.toString());
+
+        assertThat(summonerDto.getName()).isEqualTo(summonerName);
     }
 }
