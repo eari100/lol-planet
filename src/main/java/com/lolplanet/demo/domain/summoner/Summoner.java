2c7c1a6144c5df1645b53fc8500a29042b9272e8
Jaewook Choi <eari100@naver.com>
2022-01-09 19:51:27 +0900

    feat: Summoner 엔티티
    - @Getter, @NoArgsConstructor, @Builder 추가
    - BaseTimeEntity 상속

--- src/main/java/com/lolplanet/demo/domain/summoner/Summoner.java
+++ src/main/java/com/lolplanet/demo/domain/summoner/Summoner.java
@@ -1,9 +1,16 @@
 package com.lolplanet.demo.domain.summoner;
 
+import com.lolplanet.demo.domain.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
+@Getter
+@NoArgsConstructor
 @Entity
-public class Summoner {
+public class Summoner extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,4 +32,15 @@ public class Summoner {
     private String puuid;
 
     private Long summonerLevel;
+
+    @Builder
+    public Summoner(String accountId, Integer profileIconId, Long revisionDate, String name, String summonerId, String puuid, Long summonerLevel) {
+        this.accountId = accountId;
+        this.profileIconId = profileIconId;
+        this.revisionDate = revisionDate;
+        this.name = name;
+        this.summonerId = summonerId;
+        this.puuid = puuid;
+        this.summonerLevel = summonerLevel;
+    }
 }
