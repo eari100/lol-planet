28b3ade5a6e5e59a1950f538bdac6bff17b22207
Jaewook Choi <eari100@naver.com>
2022-01-09 22:31:44 +0900

    test: 소환사 정보 검색하는 API 테스트
    - case: DB에 데이터가 존재x, Riot 서버에 데이터가 존재o
    - case: DB에 데이터가 존재o, Riot 서버에 데이터가 존재o

---
+++ src/test/java/com/lolplanet/demo/web/controller/SummonerRestControllerTests.java
@@ -0,0 +1,93 @@
+package com.lolplanet.demo.web.controller;
+
+import com.lolplanet.demo.domain.summoner.Summoner;
+import com.lolplanet.demo.domain.summoner.SummonerRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DisplayName("SummonerRestController 클래스")
+public class SummonerRestControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private SummonerRepository summonerRepository;
+
+    @Nested
+    @DisplayName("findByName 메서드")
+    class findByNameTest {
+
+        @AfterEach
+        public void cleanUp() {
+            summonerRepository.deleteAll();
+        }
+
+        private final String delimitedUrl = "/lol/summoner/v4/summoners/by-name/";
+
+        @Test
+        @DisplayName(
+            "1. DB에 데이터가 존재x " +
+            "2. Riot 서버에 데이터가 존재o "
+        )
+        public void 새로운_데이터가_저장된다() throws Exception {
+            final String name = "한남동의 황제";
+
+            mockMvc.perform(get(delimitedUrl + name))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.profileIconId").exists())
+                    .andExpect(jsonPath("$.name", is(name)))
+                    .andExpect(jsonPath("$.summonerLevel", greaterThan(300)));
+        }
+
+        @Test
+        @DisplayName(
+            "1. DB에 데이터가 존재o " +
+            "2. Riot 서버에 데이터가 존재o "
+        )
+        public void Riot_서버에서_데이터를_호출되지_않고_DB에서_조회된다() throws Exception {
+            final String accountId = "vXUy9xx-2r13SGuV0gdvnJPM8hGWhAKNekdH7QHtraAi";
+            final Integer profileIconId = 4001;
+            final Long revisionDate = 1641028234000L;
+            final String name = "한남동의 황제";
+            final String summonerId = "0are3IM-Nf4gD7-wd3uVL1dGEvgsJqUQh25zU6pulSh_nHo";
+            final String puuid = "Lp57gEFg0CgfVoALp1vY3RI75siUxQGolH6k0y6X2KCjnHQPdIAbooPBMNcTw-qMn9xbQfIdCT1kng";
+            // 현재 레벨보다 낮은 레벨
+            final Long pastSummonerLevel = 100L;
+
+            Summoner summoner = Summoner.builder()
+                    .accountId(accountId)
+                    .profileIconId(profileIconId)
+                    .revisionDate(revisionDate)
+                    .name(name)
+                    .summonerId(summonerId)
+                    .puuid(puuid)
+                    .summonerLevel(pastSummonerLevel)
+                    .build();
+            summonerRepository.save(summoner);
+
+            mockMvc.perform(get(delimitedUrl + name))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.profileIconId", is(profileIconId)))
+                    .andExpect(jsonPath("$.name", is(name)))
+                    .andExpect(jsonPath("$.summonerLevel").value(pastSummonerLevel));
+        }
+    }
+}
