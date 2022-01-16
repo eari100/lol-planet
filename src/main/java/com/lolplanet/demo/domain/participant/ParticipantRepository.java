b5b7bdf94c05ab4e735c8ad84c3a69f3b3b479fc
Jaewook Choi <eari100@naver.com>
2022-01-16 13:49:49 +0900

    feat: ParticipantRepository 생성

---
+++ src/main/java/com/lolplanet/demo/domain/participant/ParticipantRepository.java
@@ -0,0 +1,6 @@
+package com.lolplanet.demo.domain.participant;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ParticipantRepository extends JpaRepository<Participant, Long> {
+}
