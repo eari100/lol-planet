743197563d16d4ea1266de5be265b24d2b8a2db4
Jaewook Choi <eari100@naver.com>
2022-01-22 19:30:22 +0900

    test: riot 서버 정보 DB에 갱신하는 API 테스트

--- src/test/java/com/lolplanet/demo/web/controller/SummonerRestControllerTests.java
+++ src/test/java/com/lolplanet/demo/web/controller/SummonerRestControllerTests.java
@@ -1,16 +1,28 @@
 package com.lolplanet.demo.web.controller;
 
+import com.lolplanet.demo.domain.match.Match;
+import com.lolplanet.demo.domain.match.MatchRepository;
+import com.lolplanet.demo.domain.participant.Participant;
+import com.lolplanet.demo.domain.participant.ParticipantRepository;
 import com.lolplanet.demo.domain.summoner.Summoner;
 import com.lolplanet.demo.domain.summoner.SummonerRepository;
-import org.junit.jupiter.api.*;
+import com.lolplanet.demo.service.MatchService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -26,14 +38,25 @@ public class SummonerRestControllerTests {
     @Autowired
     private SummonerRepository summonerRepository;
 
+    @Autowired
+    private MatchRepository matchRepository;
+
+    @Autowired
+    private ParticipantRepository participantRepository;
+
+    @Autowired
+    private MatchService matchService;
+
+    @AfterEach
+    public void cleanUp() {
+        participantRepository.deleteAll();
+        matchRepository.deleteAll();
+        summonerRepository.deleteAll();
+    }
+
     @Nested
     @DisplayName("findByName method")
     class findByNameTest {
-
-        @AfterEach
-        public void cleanUp() {
-            summonerRepository.deleteAll();
-        }
 
         private final String BASE_URL = "/lol/summoner/v4/summoners/by-name/";
 
@@ -102,5 +125,42 @@ public class SummonerRestControllerTests {
                     .andExpect(jsonPath("$.summonerLevel").doesNotExist())
                     .andExpect(status().isOk());
         }
+    }
+
+    @Test
+    public void match_participant_정보_갱신_후_조회() throws Exception {
+        final String accountId = "vXUy9xx-2r13SGuV0gdvnJPM8hGWhAKNekdH7QHtraAi";
+        final Integer profileIconId = 4001;
+        final Long revisionDate = 1641028234000L;
+        final String name = "한남동의 황제";
+        final String id = "0are3IM-Nf4gD7-wd3uVL1dGEvgsJqUQh25zU6pulSh_nHo";
+        final String puuid = "Lp57gEFg0CgfVoALp1vY3RI75siUxQGolH6k0y6X2KCjnHQPdIAbooPBMNcTw-qMn9xbQfIdCT1kng";
+        // 현재 레벨보다 낮은 레벨
+        final Long pastSummonerLevel = 100L;
+
+        Summoner summoner = Summoner.builder()
+                .accountId(accountId)
+                .profileIconId(profileIconId)
+                .revisionDate(revisionDate)
+                .name(name)
+                .id(id)
+                .puuid(puuid)
+                .summonerLevel(pastSummonerLevel)
+                .build();
+        summonerRepository.save(summoner);
+
+        mockMvc.perform(post("/lol/summoner/renew/by-name/" + name))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        Summoner updatedSummoner = summonerRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 소환사가 없습니다 id=" + id));
+        assertThat(updatedSummoner.getSummonerLevel()).isGreaterThan(pastSummonerLevel);
+
+        List<Match> matches = matchRepository.findAll();
+        assertThat(matches.size()).isGreaterThan(0);
+
+        List<Participant> participants = participantRepository.findAll();
+        assertThat(participants.size()).isGreaterThan(9); // 소환사는 최소 10명
     }
 }
