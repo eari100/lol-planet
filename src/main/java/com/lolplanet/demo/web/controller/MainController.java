be2053221d99424c4017762be0cfcf3e78bf9316
Jaewook Choi <eari100@naver.com>
2022-01-24 21:57:19 +0900

    feat: summoner-detail.html 호출하는 컨트롤러 메서드 추가

--- src/main/java/com/lolplanet/demo/web/controller/MainController.java
+++ src/main/java/com/lolplanet/demo/web/controller/MainController.java
@@ -10,4 +10,9 @@ public class MainController {
     public String main() {
         return "main";
     }
+
+    @GetMapping("/userName={name}")
+    public String summonerDetail() {
+        return "summoner-detail";
+    }
 }
