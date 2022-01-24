3d5bebca666f21a22273a8cb0e9997e25b3324dc
Jaewook Choi <eari100@naver.com>
2022-01-24 21:53:53 +0900

    feat: main 페이지
    - header, footer fragment 만든 후 적용
    - 소환사 검색 시 페이지 이동되는 스크립트 추가

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,17 @@
+const main = {
+    init: function () {
+        const _this = this
+        $('#summonerName').keydown(function(key) {
+            if (key.keyCode == 13) {
+                _this.findByName()
+                return false
+            }
+        })
+    },
+    findByName : function() {
+        const name = $('#summonerName').val()
+        window.location.href = '/userName='+name
+    }
+}
+
+main.init()
