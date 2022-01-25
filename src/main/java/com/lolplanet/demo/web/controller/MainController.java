9b7d35c256cc42fb26f95dc50f796a3dfd1cb923
Jaewook Choi <eari100@naver.com>
2022-01-25 22:19:59 +0900

    update: summoner 상세 페이지 컨트롤러 메서드에 summonerName 파라미터 전달하도록 추가

--- src/main/java/com/lolplanet/demo/web/controller/MainController.java
+++ src/main/java/com/lolplanet/demo/web/controller/MainController.java
@@ -1,7 +1,9 @@
 package com.lolplanet.demo.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class MainController {
@@ -12,7 +14,8 @@ public class MainController {
     }
 
     @GetMapping("/userName={name}")
-    public String summonerDetail() {
+    public String summonerDetail(Model model, @PathVariable(name = "name") String summonerName) {
+        model.addAttribute("summonerName", summonerName);
         return "summoner-detail";
     }
 }
