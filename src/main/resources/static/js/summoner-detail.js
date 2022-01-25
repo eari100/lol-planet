8a68de3ba0345b0a15fdab32632c89fb3030103a
Jaewook Choi <eari100@naver.com>
2022-01-25 22:21:01 +0900

    feat: Summoner 상세 페이지 Summoner 영역 렌더링 함수 추가

--- src/main/resources/static/js/summoner-detail.js
+++ src/main/resources/static/js/summoner-detail.js
@@ -1,6 +1,28 @@
 const summoner_detail = {
     init: function () {
         $( "#tabs" ).tabs()
+
+        const summonerName = $('#summonerName').val()
+        findSummonerInfo(summonerName)
+
+        function findSummonerInfo(summonerName) {
+            $.ajax({
+                type: 'GET',
+                url: '/lol/summoner/v4/summoners/by-name/' + summonerName,
+                dataType: 'json'
+            }).done(function (summoner) {
+                renderSummonerEl(summoner)
+            }).fail(function (error) {
+                alert(JSON.stringify(error))
+            })
+        }
+
+        function renderSummonerEl(summoner) {
+            $('.summoner_profile').html(`<img src="https://ddragon.leagueoflegends.com/cdn/12.2.1/img/profileicon/${summoner.profileIconId}.png" class="profile_img">`)
+            $('.summoner_name').html(`<h1>${summoner.name}</h1>`)
+            $('.summoner_level').html(`LV.${summoner.summonerLevel}`)
+        }
+
     }
 }
 
