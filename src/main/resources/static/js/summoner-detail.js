2187dd34b9d1cf746b70bf34a23415f0c29399ce
Jaewook Choi <eari100@naver.com>
2022-02-01 20:22:14 +0900

    feat: 매치 정보 렌더링 하는 함수 추가
    - ajax url template string으로 변경

--- src/main/resources/static/js/summoner-detail.js
+++ src/main/resources/static/js/summoner-detail.js
@@ -4,7 +4,7 @@ const summoner_detail = {
 
         const summonerName = $('#summonerName').val()
         findSummonerInfo()
-        findMatchList()
+        findMatchList(0)
 
         $('#btn-renew').click(function (e){
             e.preventDefault()
@@ -18,7 +18,7 @@ const summoner_detail = {
         function findSummonerInfo() {
             $.ajax({
                 type: 'GET',
-                url: '/lol/summoner/v4/summoners/by-name/' + summonerName,
+                url: `/lol/summoner/v4/summoners/by-name/${summonerName}`,
                 dataType: 'json'
             }).done(function (summoner) {
                 renderSummonerEl(summoner)
@@ -33,10 +33,10 @@ const summoner_detail = {
             $('.summoner_level').html(`LV.${summoner.summonerLevel}`)
         }
 
-        function findMatchList() {
+        function findMatchList(start) {
             $.ajax({
                 type: 'GET',
-                url: '/lol/match/list?start=0&count=20',
+                url: `/lol/match/list?start=${start}&count=20&summonerName=${summonerName}`,
                 dataType: 'json'
             }).done(function (res) {
                 res.content.forEach(match => renderMatchEl(match))
@@ -46,12 +46,171 @@ const summoner_detail = {
         }
 
         function renderMatchEl(match) {
+
+            let gameResultCss = ''
+            if(match.gameResult == '승리')
+                gameResultCss = 'Win'
+            else if(match.gameResult == '패배')
+                gameResultCss = 'Lose'
+
             $('.MatchesList').append(
                 `<div class="MatchWrap">
-                    <div class="GameItem">
+                    <div class="GameItem ${gameResultCss}">
                         <div class="Content">
                             <div class="GameStats">
                                 <div class="GameType">${match.mapName}</div>
+                                <div class="TimeStamp">
+                                    <span class="_timeago _timeCountAssigned tip">${match.gameCreationToDate}</span>
+                                </div>
+                                <div class="Bar"></div>
+                                <div class="GameResult">${match.gameResult}</div>
+                                <div class="GameLength">${match.gameDurationToString}</div>
+                            </div>
+                            <div class="GameSettingInfo">
+                                <div class="ChampionImage">
+                                    <img src="//opgg-static.akamaized.net/images/lol/champion/${match.playedChampionName}.png?image=c_scale,q_auto,w_46&amp;v=1642648372">
+                                </div>
+                                <div class="SummonerSpell">
+                                    <div class="Spell">
+                                        <img src="//opgg-static.akamaized.net/images/lol/spell/${match.usedSpell1Name}.png?image=c_scale,q_auto,w_22&amp;v=1619585878">
+                                    </div>
+                                    <div class="Spell">
+                                        <img src="//opgg-static.akamaized.net/images/lol/spell/${match.usedSpell2Name}.png?image=c_scale,q_auto,w_22&amp;v=1619585878">
+                                    </div>
+                                </div>
+                                <div class="ChampionName">${match.playedChampionName}</div>
+                            </div>
+                            <div class="KDA">
+                                <div class="KDA">
+                                    <span class="Kill">${match.kills}</span> /
+                                    <span class="Death">${match.deaths}</span> /
+                                    <span class="Assist">${match.assists}</span>
+                                </div>
+                                <div class="KDARatio">
+                                    <span class="KDARatio">${match.kda}</span>
+                                </div>
+                            </div>
+                            <div class="Stats">
+                                <div class="Level">레벨 ${match.playedChampLevel}</div>
+                                <div class="CS">
+                                    <span class="CS tip">${match.totalCs} (${match.csPerMinute})</span> CS
+                                </div>
+                            </div>
+                            <div class="Items">
+                                <div class="ItemList">
+                                    <div class="Item">
+                                        ${match.item0!=0?
+                                            `<img src="//opgg-static.akamaized.net/images/lol/item/${match.item0}.png?image=q_auto:best&amp;v=1619585878" class="Image tip">`:
+                                            `<div class="Image NoItem"></div>`
+                                        }
+                                    </div>
+                                    <div class="Item">
+                                        ${match.item1!=0?
+                                            `<img src="//opgg-static.akamaized.net/images/lol/item/${match.item1}.png?image=q_auto:best&amp;v=1619585878" class="Image tip">`:
+                                            `<div class="Image NoItem"></div>`
+                                        }
+                                    </div>
+                                    <div class="Item">
+                                        ${match.item2!=0?
+                                            `<img src="//opgg-static.akamaized.net/images/lol/item/${match.item2}.png?image=q_auto:best&amp;v=1619585878" class="Image tip">`:
+                                            `<div class="Image NoItem"></div>`
+                                        }
+                                    </div>
+                                    <div class="Item">
+                                        ${match.item6!=0?
+                                            `<img src="//opgg-static.akamaized.net/images/lol/item/${match.item6}.png?image=q_auto:best&amp;v=1619585878" class="Image tip">`:
+                                            `<div class="Image NoItem"></div>`
+                                        }
+                                    </div>
+                                    <div class="Item">
+                                        ${match.item3!=0?
+                                            `<img src="//opgg-static.akamaized.net/images/lol/item/${match.item3}.png?image=q_auto:best&amp;v=1619585878" class="Image tip">`:
+                                            `<div class="Image NoItem"></div>`
+                                        }
+                                    </div>
+                                    <div class="Item">
+                                        ${match.item4!=0?
+                                            `<img src="//opgg-static.akamaized.net/images/lol/item/${match.item4}.png?image=q_auto:best&amp;v=1619585878" class="Image tip">`:
+                                            `<div class="Image NoItem"></div>`
+                                        }
+                                    </div>
+                                    <div class="Item">
+                                        ${match.item5!=0?
+                                            `<img src="//opgg-static.akamaized.net/images/lol/item/${match.item5}.png?image=q_auto:best&amp;v=1619585878" class="Image tip">`:
+                                            `<div class="Image NoItem"></div>`
+                                        }
+                                    </div>   
+                                </div>
+                                <div class="Trinket">
+                                    <img src="//opgg-static.akamaized.net/images/site/summoner/icon-ward-blue.png">
+                                    제어 와드 <span class="wards vision">${match.visionWardsBoughtInGame}</span>
+                                </div>
+                            </div>
+                            <div class="FollowPlayers Names">
+                                <div class="Team">
+                                    <div class="Summoner ">
+                                        <div class="ChampionImage">
+                                            <img src="//opgg-static.akamaized.net/images/lol/champion/${match.participants[0].championName}.png?image=c_scale,q_auto,w_46&amp;v=1619585878" class="Image16 __sprite __spc16 __spc16-34 tip tpd-delegation-uid-1">
+                                        </div>
+                                        <div class="SummonerName">${match.participants[0].summonerName}</div>
+                                    </div>
+                                    <div class="Summoner ">
+                                        <div class="ChampionImage">
+                                            <img src="//opgg-static.akamaized.net/images/lol/champion/${match.participants[1].championName}.png?image=c_scale,q_auto,w_46&amp;v=1619585878" class="Image16 __sprite __spc16 __spc16-34 tip tpd-delegation-uid-1">
+                                        </div>
+                                        <div class="SummonerName">${match.participants[1].summonerName}</div>
+                                    </div>
+                                    <div class="Summoner ">
+                                        <div class="ChampionImage">
+                                            <img src="//opgg-static.akamaized.net/images/lol/champion/${match.participants[2].championName}.png?image=c_scale,q_auto,w_46&amp;v=1619585878" class="Image16 __sprite __spc16 __spc16-34 tip tpd-delegation-uid-1">
+                                        </div>
+                                        <div class="SummonerName">${match.participants[2].summonerName}</div>
+                                    </div>
+                                    <div class="Summoner ">
+                                        <div class="ChampionImage">
+                                            <img src="//opgg-static.akamaized.net/images/lol/champion/${match.participants[3].championName}.png?image=c_scale,q_auto,w_46&amp;v=1619585878" class="Image16 __sprite __spc16 __spc16-34 tip tpd-delegation-uid-1">
+                                        </div>
+                                        <div class="SummonerName">${match.participants[3].summonerName}</div>
+                                    </div>
+                                    <div class="Summoner ">
+                                        <div class="ChampionImage">
+                                            <img src="//opgg-static.akamaized.net/images/lol/champion/${match.participants[4].championName}.png?image=c_scale,q_auto,w_46&amp;v=1619585878" class="Image16 __sprite __spc16 __spc16-34 tip tpd-delegation-uid-1">
+                                        </div>
+                                        <div class="SummonerName">${match.participants[4].summonerName}</div>
+                                    </div>
+                                </div>
+                                <div class="Team">
+                                    <div class="Summoner ">
+                                        <div class="ChampionImage">
+                                            <img src="//opgg-static.akamaized.net/images/lol/champion/${match.participants[5].championName}.png?image=c_scale,q_auto,w_46&amp;v=1619585878" class="Image16 __sprite __spc16 __spc16-34 tip tpd-delegation-uid-1">
+                                        </div>
+                                        <div class="SummonerName">${match.participants[5].summonerName}</div>
+                                    </div>
+                                    <div class="Summoner ">
+                                        <div class="ChampionImage">
+                                            <img src="//opgg-static.akamaized.net/images/lol/champion/${match.participants[6].championName}.png?image=c_scale,q_auto,w_46&amp;v=1619585878" class="Image16 __sprite __spc16 __spc16-34 tip tpd-delegation-uid-1">
+                                        </div>
+                                        <div class="SummonerName">${match.participants[6].summonerName}</div>
+                                    </div>
+                                    <div class="Summoner ">
+                                        <div class="ChampionImage">
+                                            <img src="//opgg-static.akamaized.net/images/lol/champion/${match.participants[7].championName}.png?image=c_scale,q_auto,w_46&amp;v=1619585878" class="Image16 __sprite __spc16 __spc16-34 tip tpd-delegation-uid-1">
+                                        </div>
+                                        <div class="SummonerName">${match.participants[7].summonerName}</div>
+                                    </div>
+                                    <div class="Summoner ">
+                                        <div class="ChampionImage">
+                                            <img src="//opgg-static.akamaized.net/images/lol/champion/${match.participants[8].championName}.png?image=c_scale,q_auto,w_46&amp;v=1619585878" class="Image16 __sprite __spc16 __spc16-34 tip tpd-delegation-uid-1">
+                                        </div>
+                                        <div class="SummonerName">${match.participants[8].summonerName}</div>
+                                    </div>
+                                    <div class="Summoner ">
+                                        <div class="ChampionImage">
+                                            <img src="//opgg-static.akamaized.net/images/lol/champion/${match.participants[9].championName}.png?image=c_scale,q_auto,w_46&amp;v=1619585878" class="Image16 __sprite __spc16 __spc16-34 tip tpd-delegation-uid-1">
+                                        </div>
+                                        <div class="SummonerName">${match.participants[9].summonerName}</div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -61,14 +220,14 @@ const summoner_detail = {
         function renew(ladda) {
            $.ajax({
                type: 'POST',
-               url: '/lol/summoner/renew/by-name/' + summonerName,
+               url: `/lol/summoner/renew/by-name/${summonerName}`,
                success: function () {
                    ladda.setProgress(0.9)
                }
            }).done(function () {
                $('.MatchesList').empty()
                findSummonerInfo()
-               findMatchList()
+               findMatchList(0)
            }).fail(function (error) {
                alert(JSON.stringify(error))
            }).always(function() {
