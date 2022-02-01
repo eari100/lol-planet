66966dce961376ff29d2be88354d26e7d61d61b5
Jaewook Choi <eari100@naver.com>
2022-02-01 19:32:39 +0900

    feat: MatchResDto 아이템, 구매한 제어와드 수 추가

--- src/main/java/com/lolplanet/demo/web/dto/MatchResDto.java
+++ src/main/java/com/lolplanet/demo/web/dto/MatchResDto.java
@@ -26,6 +26,14 @@ public class MatchResDto {
     private Integer neutralMinionsKilled;
     private Integer totalCs;
     private String csPerMinute;
+    private Integer item0;
+    private Integer item1;
+    private Integer item2;
+    private Integer item3;
+    private Integer item4;
+    private Integer item5;
+    private Integer item6;
+    private Integer visionWardsBoughtInGame;
     private List<ParticipantResDto> participantResDtos = new ArrayList<>();
 
     public MatchResDto(Match entity, String summonerName) {
@@ -53,6 +61,14 @@ public class MatchResDto {
                 this.neutralMinionsKilled = participant.getNeutralMinionsKilled();
                 this.totalCs = participant.getTotalMinionsKilled() + participant.getNeutralMinionsKilled();
                 this.csPerMinute = converter.convertToCsPerMinute(this.totalCs, entity.getGameDuration());
+                this.item0 = participant.getItem0();
+                this.item1 = participant.getItem1();
+                this.item2 = participant.getItem2();
+                this.item3 = participant.getItem3();
+                this.item4 = participant.getItem4();
+                this.item5 = participant.getItem5();
+                this.item6 = participant.getItem6();
+                this.visionWardsBoughtInGame = participant.getVisionWardsBoughtInGame();
             }
         }
     }
