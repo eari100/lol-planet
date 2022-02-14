390a7f38ce8c74b223e8edecad71a9a38e5bb4c8
Jaewook Choi <eari100@naver.com>
2022-02-14 22:45:08 +0900

    test: 모든 테스트 코드에 @ActiveProfiles 추가

--- src/test/java/com/lolplanet/demo/domain/MatchRepositoryTests.java
+++ src/test/java/com/lolplanet/demo/domain/MatchRepositoryTests.java
@@ -11,12 +11,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@ActiveProfiles("test")
 @SpringBootTest
 public class MatchRepositoryTests {
 
